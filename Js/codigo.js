83b391c38b0197f1dd99043ebc8f10727ae34404
CubitoEstudioso <ig.lopezf@duocuc.cl>
2025-09-22 18:27:58 -0300

    Se arregla el registro y se agregan validaciones con JavaScript

---
+++ Js/codigo.js
@@ -0,0 +1,105 @@
+let cuentas = new Map([
+  ["ig.lopezf@duocuc.cl", "12345"],
+  ["usuariogenerico@gmail.com", "54321"]
+])
+
+const regiones = ["Región Metropolitana de Santiago", "Región de Arica y Parinacota", "Región de Tarapacá", "Región de Antofagasta",
+  "Región de Atacama", "Región de Coquimbo", "Región de Valparaíso", "Región de O'Higgins", "Región del Maule", "Región del Ñuble",
+  "Región del Biobío", "Región de la Araucanía", "Región de los Ríos", "Región de los Lagos","Región de Aysén", "Región de Magallanes"]
+
+const arica = ["Arica", "Camarones", "General Lagos", "Putre"]
+const tarapaca = ["Alto Hospicio", "Camiña", "Colchane", "Huara", "Iquique", "Pica", "Pozo Almonte"]
+const antofagasta = ["Antofagasta", "Calama", "María Elena", "Mejillones", "Ollagüe", "San Pedro de Atacama", "Sierra Gorda", "Taltal", "Tocopilla"]
+const atacama = ["Alto del Carmen", "Caldera", "Chañaral", "Copiapó", "Diego de Almagro", "Freirina", "Huasco", "Tierra Amarilla", "Vallenar"]
+const coquimbo = ["Andacollo", "Canela", "Combarbalá", "Coquimbo", "Illapel", "La Higuera", "La Serena", "Los Vilos", "Monte Patria", "Ovalle",
+  "Paihuano", "Punitaqui", "Río Hurtado", "Salamanca", "Vicuña"]
+const valparaiso = ["Algarrobo", "Cabildo", "Calle Larga", "Cartagena", "Casablanca", "Catemu", "Concón", "El Quisco", "El Tabo", "Hijuelas",
+  "Isla de Pascua", "Juan Fernández", "La Calera", "La Cruz", "La Ligua", "Limache", "Llaillay", "Los Andes", "Nogales", "Olmué", "Panquehue",
+  "Papudo", "Petorca", "Puchuncaví", "Putaendo", "Quillota", "Quilpué", "Quintero", "Rinconada", "San Antonio", "San Esteban", "San Felipe",
+  "Santa María", "Santo Domingo", "Valparaíso", "Villa Alemana", "Viña del Mar", "Zapallar"]
+const santiago = ["Alhué", "Buin", "Calera de Tango", "Cerrillos", "Cerro Navia", "Colina", "Conchalí", "Curacaví", "El Bosque", "El Monte",
+  "Estación Central", "Huechuraba", "Independencia", "Isla de Maipo", "La Cisterna", "La Florida", "La Granja", "Lampa", "La Pintana", "La Reina",
+  "Las Condes", "Lo Barnechea", "Lo Espejo", "Lo Prado", "Macul", "Maipú", "María Pinto", "Melipilla", "Ñuñoa", "Padre Hurtado", "Paine",
+  "Pedro Aguirre Cerda", "Peñaflor", "Peñalolén", "Pirque", "Providencia", "Pudahuel", "Puente Alto", "Quilicura", "Quinta Normal", "Recoleta",
+  "Renca", "San Bernardo", "San Joaquín", "San José de Maipo", "San Miguel", "San Pedro", "San Ramón", "Santiago", "Talagante", "Tiltil", "Vitacura"]
+const ohiggins = ["Chépica", "Chimbarongo", "Codegua", "Coinco", "Coltauco", "Doñihue", "Graneros", "La Estrella", "Las Cabras", "Litueche", "Lolol",
+  "Machalí", "Malloa", "Marchihue", "Mostazal", "Nancagua", "Navidad", "Olivar", "Palmilla", "Paredones", "Peralillo", "Peumo", "Pichidegua",
+  "Pichilemu", "Placilla", "Pumanque", "Quinta de Tilcoco", "Rancagua", "Rengo", "Requínoa", "San Fernando", "Santa Cruz", "San Vicente"]
+const maule = ["Cauquenes", "Chanco", "Colbún", "Constitución", "Curepto", "Curicó", "Empedrado", "Hualañe", "Licantén", "Linares", "Longaví", "Maule",
+  "Molina", "Parral", "Pelarco", "Pelluhue", "Pencahue", "Rauco", "Retiro", "Río Claro", "Romeral", "Sagrada Familia", "San Clemente", "San Javier",
+  "San Rafael", "Talca", "Teno", "Vichuquén", "Villa Alegre", "Yerbas Buenas"]
+const nuble = ["Bulnes", "Chillán", "Chillán Viejo", "Cobquecura", "Coelemu", "Coihueco", "El Carmen", "Ninhue", "Ñiquén", "Pemuco", "Pinto",
+  "Portezuelo", "Quillón", "Quirihue", "Ránquil", "San Carlos", "San Fabián", "San Ignacio", "San Nicolás", "Treguaco", "Yungay"]
+const biobio = ["Alto Biobío", "Antuco", "Arauco", "Cabrero", "Cañete", "Chiguayante", "Concepción", "Contulmo", "Coronel", "Curanilahue", "Florida",
+  "Hualpén", "Hualqui", "Laja", "Lebu", "Los Alamos", "Los Angeles", "Lota", "Mulchén", "Nacimiento", "Negrete", "Penco", "Quilaco", "Quilleco",
+  "San Pedro de la Paz", "San Rosendo", "Santa Bárbara", "Santa Juana", "Talcahuano", "Tirúa", "Tomé", "Tucapel", "Yumbel"]
+const araucania = ["Angol", "Carahue", "Cholchol", "Collipulli", "Cunco", "Curacautín", "Curarrehue", "Ercilla", "Freire", "Galvarino", "Gorbea",
+  "Lautaro", "Loncoche", "Lonquimay", "Los Sauces", "Lumaco", "Melipeuco", "Nueva Imperial", "Padre Las Casas", "Perquenco", "Pitrufquén", "Pucón",
+  "Purén", "Renaico", "Saavedra", "Temuco", "Teodoro Schmidt", "Toltén", "Traiguén", "Victoria", "Vilcún", "Villarrica"]
+const rios = ["Corral", "Futrono", "Lago Ranco", "Lanco", "La Unión", "Los Lagos", "Máfil", "Mariquina", "Paillaco", "Panguipulli", "Río Bueno", "Valdivia"]
+const lagos = ["Ancud", "Calbuco", "Castro", "Chaitén", "Chonchi", "Cochamó", "Curaco de Vélez", "Dalcahue", "Fresia", "Frutillar", "Futaleufú",
+  "Hualaihué", "Llanquihue", "Los Muermos", "Maullín", "Osorno", "Palena", "Puerto Montt", "Puerto Octay", "Puerto Varas", "Puqueldón", "Purranque",
+  "Puyehue", "Queilén", "Quellón", "Quemchi", "Quinchao", "Río Negro", "San Juan de la Costa", "San Pablo"]
+const aysen = ["Aysén", "Chile Chico", "Cisnes", "Cochrane", "Coyhaique", "Guaitecas", "Lago Verde", "O'Higgins", "Río Ibáñez", "Tortel"]
+const magallanes = ["Antártica", "Cabo de Hornos", "Laguna Blanca", "Natales", "Porvenir", "Primavera", "Punta Arenas", "Río Verde", "San Gregorio",
+  "Timaukel", "Torres del Paine"]
+
+const comunasPorRegion = {
+  "Región Metropolitana de Santiago": santiago,
+  "Región de Arica y Parinacota": arica,
+  "Región de Tarapacá": tarapaca,
+  "Región de Antofagasta": antofagasta,
+  "Región de Atacama": atacama,
+  "Región de Coquimbo": coquimbo,
+  "Región de Valparaíso": valparaiso,
+  "Región de O'Higgins": ohiggins,
+  "Región del Maule": maule,
+  "Región del Ñuble": nuble,
+  "Región del Biobío": biobio,
+  "Región de la Araucanía": araucania,
+  "Región de los Ríos": rios,
+  "Región de los Lagos": lagos,
+  "Región de Aysén": aysen,
+  "Región de Magallanes": magallanes
+}
+
+function mostrarRegiones() {
+  const select = document.getElementById("region")
+
+  regiones.forEach(region => {
+    const opcion = document.createElement("option")
+    opcion.value = region
+    opcion.textContent = region
+    select.appendChild(opcion)
+  })
+}
+
+function mostrarComunas() {
+  const regionSeleccionada = document.getElementById("region")
+  const comunaSeleccionada = document.getElementById("comuna")
+
+  const region = regionSeleccionada.value
+  comunaSeleccionada.innerHTML = "<option value=''>Seleccione una comuna</option>"
+
+  if (!region || !comunasPorRegion[region]) {
+    comunaSeleccionada.disabled = true
+    return
+  }
+
+  const comunas = comunasPorRegion[region]
+
+  comunas.forEach(comuna => {
+    const opcion = document.createElement("option")
+    opcion.value = comuna
+    opcion.textContent = comuna
+    comunaSeleccionada.appendChild(opcion)
+  })
+
+  comunaSeleccionada.disabled = false;
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+  mostrarRegiones(
+  document.getElementById("region").addEventListener("change", mostrarComunas)
+  )
+})
